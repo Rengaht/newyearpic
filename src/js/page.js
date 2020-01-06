b053c05c572e8692c57cd29a29c1e2a9f2eb4e39
Reng Tsai <rengah0@gmail.com>
2020-01-06 19:37:33 +0800

    update

--- src/js/page.js
+++ src/js/page.js
@@ -1,9 +1,10 @@
 
 var _websocket;
-
+var _speah_recognition;
 window.onload=function(){
 	
 	connectToWebsocket();
+	startSpeachRecognition();
 
 	$('#_text_wish').bind("change keyup",function(){
 		sendTextUpdate();
@@ -27,4 +28,50 @@ function connectToWebsocket(){
 function sendTextUpdate(){
 	var text_=$('#_text_wish').val();
 	_websocket.send(text_);
+}
+
+function startSpeachRecognition(){
+	if(!('webkitSpeechRecognition' in window)){
+		console.log("No Speach Recognition Supported!!!");
+		return;
+  	}
+	_speah_recognition=new webkitSpeechRecognition();
+	_speah_recognition.lang="cmn-Hant-TW";
+	_speah_recognition.continuous=true;
+	_speah_recognition.interimResults=true;
+
+	_speah_recognition.onstart=onSpeachRecognitionStart;
+	_speah_recognition.onend=onSpeachRecognitionStop;
+	_speah_recognition.onresult=onSpeachRecognitionResult;
+}
+function onSpeachRecognitionStart(){
+	console.log("start speach...");
+}
+function onSpeachRecognitionStop(){
+	console.log("stop speach...");
+}
+function onSpeachRecognitionResult(event){
+	console.log(event);
+
+	//var i=event.resultIndex;
+  	//var j=event.results[i].length-1;
+
+  	var text_="";
+
+  	for(var i=0;i<event.results.length;++i){
+  		text_+=event.results[i][0].transcript;	
+  	}
+
+  	$('#_text_wish').val(text_);
+
+ 	_websocket.send(text_); 
+ 	
+ 	// TODO: check length
+
+}
+function startRecognition(){
+	_speah_recognition.start();
+}
+function stopRecognition(){
+	_speah_recognition.stop();
 }
